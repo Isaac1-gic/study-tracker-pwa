391b5c238eb28259f8cf5c886972408cfd03372c
Isaac1-gic <isaacchitsakamire@gmail.com>
2025-12-28 14:50:48 +0200

    correcting app.js

--- js/app.js
+++ js/app.js
@@ -2173,7 +2173,7 @@ document.getElementById('prompt-container-ai').addEventListener('click', functio
             all.forEach(rem =>{
                 scheduleInPage(rem);
             })
-        }cache(e){}
+        }catch(e){}
     }
 
     async function createReminder(title, body, timeISO, repeat=null) {
